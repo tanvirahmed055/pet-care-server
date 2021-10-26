020b27f97c77b62166bd9505b4832bce9f9e14d4
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-10-26 19:15:40 +0600

    developed GET api for services

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
 const express = require('express')
 const { MongoClient } = require('mongodb');
+const cors = require('cors')
+require('dotenv').config();
 const app = express()
 const port = 5000
 
+//middleware
+app.use(cors())
+app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oa9tu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    console.log("yee connected");
-});
+
+
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("pet_care_database");
+        const servicesCollection = database.collection("services");
+
+
+        //GET API for services
+        app.get('/services', async (req, res) => {
+            const query = {};
+
+            const cursor = servicesCollection.find(query);
+            // print a message if no documents were found
+            if ((await cursor.count()) === 0) {
+                console.log("No documents found!");
+            }
+            // replace console.dir with your callback to access individual elements
+            const services = await cursor.toArray();
+            res.json(services);
+        })
+
+    } finally {
+        //await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 
 
 app.get('/', (req, res) => {
